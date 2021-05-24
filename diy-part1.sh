3f8ea4d1adc68eda2774644ec1287e51ca3bdb53
xwdj <40928432+1787755656@users.noreply.github.com>
2021-05-24 18:02:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git 1787755656 https://github.com/1787755656/openwrt' feeds.conf.default
+sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
